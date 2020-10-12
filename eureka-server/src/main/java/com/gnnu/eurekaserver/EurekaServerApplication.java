60b7a7cbf4ba407acbbb96c180fceba3a709fb52
hongjiahua <www.2640601916@outlook.com>
2020-10-12 09:45:47 +0800

    相关证书

--- eureka-server/src/main/java/com/gnnu/eurekaserver/EurekaServerApplication.java
+++ eureka-server/src/main/java/com/gnnu/eurekaserver/EurekaServerApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class EurekaServerApplication {
+public class EurekaServerApplication {//哈哈哈
 
     public static void main(String[] args) {
         SpringApplication.run(EurekaServerApplication.class, args);
